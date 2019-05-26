539baae36308d63918a23741322aa11f1333e821
Nicholas Pung <nckpng95@gmail.com>
2019-05-26 11:25:30 -0700

    started playing around with dates  for calendar

--- calendar.js
+++ calendar.js
@@ -1,1 +1,18 @@
-console.log("hello there");
+// grabbing browser dates
+
+let today = new Date();
+
+let todayDay = today.getDay();
+let todayDate = today.getDate();
+let todayMonth = today.getMonth();
+let todayYear = today.getFullYear();
+
+console.log('Day', todayDay);
+console.log('Date', todayDate);
+console.log('Month', todayMonth);
+console.log('Year', todayYear);
+
+// creating a calendar template
+
+let { january, march, may, july, august, october, december }= 31;
+console.log(january);
