4808e636837bf1af09ca39825528e64a906c37b1
Nicholas Pung <nckpng95@gmail.com>
2019-05-26 12:02:29 -0700

    added dates for the year

--- calendar.js
+++ calendar.js
@@ -13,6 +13,13 @@ console.log('Month', todayMonth);
 console.log('Year', todayYear);
 
 // creating a calendar template
+let days31 = 31;
+let days30 = 30;
+let days29 = 29;
+let days28 = 28;
 
-let { january, march, may, july, august, october, december }= 31;
-console.log(january);
+let january = days31, march = days31, may = days31, july = days31, august = days31, october = days31, december = days31;
+let april = days30, june = days30, september = days30, november = days30;
+let february = days28;
+
+let year = [january, february, march, april, may, june, july, august, september, october, november, december];
